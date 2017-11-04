776efe231c2b16c9f62bcc42377d640b4bfca191
Noah-Dev <ark_code@protonmail.com>
2017-11-04 18:15:26 -0500

    Preparing to add scaffoled API

---
+++ SignInMP_00/Models/Member.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SignInMP_00.Models
+{
+    public class Member
+    {
+        public int Id { get; set; }
+        public int DateTimeUnix { get; set; }
+    }
+}
---
+++ SignInMP_00/Models/SignInDB.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace SignInMP_00.Models
+{
+    public class SignInDB : DbContext
+    {
+        public SignInDB() : base("SignInDB")
+        {
+
+        }
+
+        public DbSet<Member> Members {get; set;}
+
+        public DbSet<SignIn> SignIns { get; set; }
+    }
+}
